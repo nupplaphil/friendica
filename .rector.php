c2b1e20916cc57d47ccdcc88100906d83c835f70
Art4 <art4@wlabs.de>
2026-04-30 22:35:38 +0200

    test: set all dataprovider to static using rector StaticDataProviderClassMethodRector

--- .rector.php
+++ .rector.php
@@ -24,7 +24,7 @@ return \Rector\Config\RectorConfig::configure()
 	// ->withCodeQualityLevel(0)
 	->withPhpLevel(84)
 	->withSets([
-		//\Rector\Set\ValueObject\LevelSetList::UP_TO_PHP_85,
+		\Rector\Set\ValueObject\LevelSetList::UP_TO_PHP_74,
 		\Rector\PHPUnit\Set\PHPUnitSetList::PHPUNIT_100,
 	])
 	->withSkip([
