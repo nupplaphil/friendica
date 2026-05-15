b8915d0506307c8a213ff84f0bb606c838ad7896
Art4 <art4@wlabs.de>
2026-05-15 19:51:52 +0200

    fix: fix phpstan error

--- src/Module/Settings/ContactImport.php
+++ src/Module/Settings/ContactImport.php
@@ -38,6 +38,8 @@ class ContactImport extends BaseSettings
 	public function __construct(private ICanSendHttpRequests $httpClient, SystemMessages $systemMessages, private IManageConfigValues $config, IHandleUserSessions $session, App\Page $page, L10n $l10n, App\BaseURL $baseUrl, App\Arguments $args, LoggerInterface $logger, Profiler $profiler, Response $response, array $server, array $parameters = [])
 	{
 		parent::__construct($session, $page, $l10n, $baseUrl, $args, $logger, $profiler, $response, $server, $parameters);
+
+		$this->systemMessages = $systemMessages;
 	}
 
 	protected function post(array $request = [])
