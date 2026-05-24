2bea29d203cefb2651f3b6f02a1f10c1392d0b81
Art4 <art4@wlabs.de>
2026-05-24 07:59:31 +0200

    refactor: fix errors in src/Util/HTTPHeaders.php

--- src/Util/HTTPHeaders.php
+++ src/Util/HTTPHeaders.php
@@ -19,28 +19,26 @@ class HTTPHeaders
 	{
 		$lines = explode("\n", str_replace("\r", '', $headers));
 
-		if ($lines) {
-			foreach ($lines as $line) {
-				if (preg_match('/^\s+/', $line, $matches) && trim($line)) {
-					if (!empty($this->in_progress['k'])) {
-						$this->in_progress['v'] .= ' ' . ltrim($line);
-						continue;
-					}
-				} else {
-					if (!empty($this->in_progress['k'])) {
-						$this->parsed[] = [$this->in_progress['k'] => $this->in_progress['v']];
-						$this->in_progress = [];
-					}
-
-					$this->in_progress['k'] = strtolower(substr($line, 0, strpos($line, ':')));
-					$this->in_progress['v'] = ltrim(substr($line, strpos($line, ':') + 1));
+		foreach ($lines as $line) {
+			if (preg_match('/^\s+/', $line, $matches) && trim($line)) {
+				if (!empty($this->in_progress['k'])) {
+					$this->in_progress['v'] .= ' ' . ltrim($line);
+					continue;
 				}
-			}
+			} else {
+				if (!empty($this->in_progress['k'])) {
+					$this->parsed[] = [$this->in_progress['k'] => $this->in_progress['v']];
+					$this->in_progress = [];
+				}
 
-			if (!empty($this->in_progress['k'])) {
-				$this->parsed[$this->in_progress['k']] = $this->in_progress['v'];
-				$this->in_progress = [];
+				$this->in_progress['k'] = strtolower(substr($line, 0, strpos($line, ':')));
+				$this->in_progress['v'] = ltrim(substr($line, strpos($line, ':') + 1));
 			}
+		}
+
+		if (!empty($this->in_progress['k'])) {
+			$this->parsed[$this->in_progress['k']] = $this->in_progress['v'];
+			$this->in_progress = [];
 		}
 	}
 
