a20b156b101f6928cc3318039a9b2d2158c5ba0e
nupplaPhil <no-reply@friendi.ca>
2026-06-08 20:31:53 +0200

    Move DependencyCheckTest.php from Functional to Integration

--- tests/Integration/DependencyCheckTest.php
+++ tests/Integration/DependencyCheckTest.php
@@ -5,7 +5,7 @@
 //
 // SPDX-License-Identifier: AGPL-3.0-or-later
 
-namespace Friendica\Test\Functional;
+namespace Friendica\Test\Integration;
 
 use Friendica\App;
 use Friendica\Core\Cache\Capability\ICanCache;
