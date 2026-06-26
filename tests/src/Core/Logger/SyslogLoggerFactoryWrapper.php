0f46d1713f228051642da3d05f8f985e00cbe9a5
Art4 <art4@wlabs.de>
2026-06-26 15:18:15 +0200

    chore: rectify code

--- tests/src/Core/Logger/SyslogLoggerFactoryWrapper.php
+++ tests/src/Core/Logger/SyslogLoggerFactoryWrapper.php
@@ -11,13 +11,12 @@ use Friendica\Core\Config\Capability\IManageConfigValues;
 use Friendica\Core\Logger\Exception\LogLevelException;
 use Friendica\Core\Logger\Factory\SyslogLogger;
 use Friendica\Core\Logger\Type\SyslogLogger as SyslogLoggerClass;
-use Psr\Log\LoggerInterface;
 
 class SyslogLoggerFactoryWrapper extends SyslogLogger
 {
 	public function create(IManageConfigValues $config): SyslogLoggerWrapper
 	{
-		$logOpts     = (int) ($config->get('system', 'syslog_flags')    ?? SyslogLoggerClass::DEFAULT_FLAGS);
+		$logOpts     = (int) ($config->get('system', 'syslog_flags') ?? SyslogLoggerClass::DEFAULT_FLAGS);
 		$logFacility = (int) ($config->get('system', 'syslog_facility') ?? SyslogLoggerClass::DEFAULT_FACILITY);
 		$loglevel    = SyslogLogger::mapLegacyConfigDebugLevel($config->get('system', 'loglevel'));
 
