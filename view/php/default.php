dc79d14f7e70affa70ff0198ed606a3a19f22c1c
nupplaPhil <no-reply@friendi.ca>
2026-07-21 20:52:15 +0200

    PHP-CS : Fix codestyle

--- view/php/default.php
+++ view/php/default.php
@@ -14,19 +14,33 @@
 <!DOCTYPE html>
 <html itemscope itemtype="http://schema.org/Blog" lang="<?php echo $lang; ?>">
 <head>
-  <title><?php if(!empty($page['title'])) echo $page['title'] ?></title>
+  <title><?php if (!empty($page['title'])) {
+  	echo $page['title'];
+  } ?></title>
   <script>var baseurl="<?php echo Friendica\DI::baseUrl() ?>";</script>
-  <?php if(!empty($page['htmlhead'])) echo $page['htmlhead'] ?>
+  <?php if (!empty($page['htmlhead'])) {
+  	echo $page['htmlhead'];
+  } ?>
 </head>
 <body class="mod-<?php echo $page['module'] ?>">
-	<?php if(!empty($page['nav'])) echo $page['nav']; ?>
-	<aside><?php if(!empty($page['aside'])) echo $page['aside']; ?></aside>
+	<?php if (!empty($page['nav'])) {
+		echo $page['nav'];
+	} ?>
+	<aside><?php if (!empty($page['aside'])) {
+		echo $page['aside'];
+	} ?></aside>
 	<section>
-		<?php if(!empty($page['content'])) echo $page['content']; ?>
+		<?php if (!empty($page['content'])) {
+			echo $page['content'];
+		} ?>
 		<div id="pause"></div> <!-- The pause/resume Ajax indicator -->
 		<div id="page-footer"></div>
 	</section>
-	<right_aside><?php if(!empty($page['right_aside'])) echo $page['right_aside']; ?></right_aside>
-	<footer><?php if(!empty($page['footer'])) echo $page['footer']; ?></footer>
+	<right_aside><?php if (!empty($page['right_aside'])) {
+		echo $page['right_aside'];
+	} ?></right_aside>
+	<footer><?php if (!empty($page['footer'])) {
+		echo $page['footer'];
+	} ?></footer>
 </body>
 </html>
