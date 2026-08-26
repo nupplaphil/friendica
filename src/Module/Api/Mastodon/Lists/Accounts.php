a802eadcebcdf8384353dadaa151873959dedccd
nupplaPhil <no-reply@friendi.ca>
2026-08-26 23:35:59 +0200

    Use Circle::exists for the remaining circle lookups in the API
    The inline checks added in #16072 repeated the condition instead of
    asking the helper.
    Circle::exists now also excludes deleted circles there.

--- src/Module/Api/Mastodon/Lists/Accounts.php
+++ src/Module/Api/Mastodon/Lists/Accounts.php
@@ -32,7 +32,7 @@ class Accounts extends BaseApi
 			$this->logAndJsonError(422, $this->errorFactory->UnprocessableEntity());
 		}
 
-		if (!DBA::exists('group', ['id' => $this->parameters['id'], 'uid' => $uid])) {
+		if (!Circle::exists((int) $this->parameters['id'], $uid)) {
 			$this->logAndJsonError(404, $this->errorFactory->RecordNotFound());
 		}
 
@@ -52,7 +52,7 @@ class Accounts extends BaseApi
 			$this->logAndJsonError(422, $this->errorFactory->UnprocessableEntity());
 		}
 
-		if (!DBA::exists('group', ['id' => $this->parameters['id'], 'uid' => $uid])) {
+		if (!Circle::exists((int) $this->parameters['id'], $uid)) {
 			$this->logAndJsonError(404, $this->errorFactory->RecordNotFound());
 		}
 
